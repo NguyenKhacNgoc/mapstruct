c50e2fb8166bde48d638553f63fb0553f42d9625
NguyenKhacNgoc <khacngoc6969@gmail.com>
2024-10-09 09:05:39 +0700

    update url

--- src/main/java/com/example/mapstruct/IdentityClient.java
+++ src/main/java/com/example/mapstruct/IdentityClient.java
@@ -3,7 +3,7 @@ package com.example.mapstruct;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "identity-client", url = "http://localhost:8888")
+@FeignClient(name = "identity-client", url = "https://openfeign.onrender.com")
 public interface IdentityClient
 {
     @GetMapping("/api/test")
